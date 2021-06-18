9821a41f02e602b80eebf3c3d8829fe329191b8a
Hiroaki Ikeda <fusianasan.jimso@gmail.com>
2021-06-19 03:52:32 +0900

    アドレス変更

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -26,8 +26,8 @@ const Layout = ({pageTitle,children}) => {
       <nav>
         <ul className={navLinks}>
           <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
-          <li className={navLinkItem}><Link to="about" className={navLinkText}>About</Link></li>
-          <li className={navLinkItem}><Link to="blog" className={navLinkText}>Blog</Link></li>
+          <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
+          <li className={navLinkItem}><Link to="/blog" className={navLinkText}>Blog</Link></li>
         </ul>
         <h1 className={heading}>{pageTitle}</h1>
         {children}
